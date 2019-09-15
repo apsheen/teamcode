a24a5fb7986dc7d360a5c17405bcb9238bda24b7
ansley <ainsleysheen@gmail.com>
2019-09-15 10:12:32 -0700

    first code

---
+++ autonomous.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class autonomous extends LinearOpMode {
+    //TODO
+
+    public void runOpMode() {
+        //TODO
+    }
+}
