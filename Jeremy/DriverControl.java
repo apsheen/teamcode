742fed6817a53917e17fdf68f798fb87bbbffd89
J <41495824+Jedite@users.noreply.github.com>
2019-10-06 16:33:43 -0700

    10/6 update

--- Jeremy/DriverControl.java
+++ Jeremy/DriverControl.java
@@ -4,86 +4,95 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import java.util.Math;
 
 @TeleOp
 //will display this opmode in the driver station
-
 public class DriverControl extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime(); //game timer
 
-    Robot mRobot = new Robot();
+    //motor objects -- private
+    private DcMotor leftFront;
+    private DcMotor leftBack;
+    private DcMotor rightFront;
+    private DcMotor rightBack;
+
+    double v1, v2, v3, v4;
+    double liftLeftV, liftRightV;
+
+    private DcMotor liftLeft;
+    private DcMotor liftRight;
+
+    public void runMechanumVectorMethod() {
+        double driveV, strafeV, rotateV;
+
+        driveV = this.gamepad1.left_stick_y;
+        strafeV = -this.gamepad1.left_stick_x;
+        rotateV = this.gamepad1.right_stick_x;
+
+        v1 = driveV + strafeV + rotateV;
+        v2 = driveV - strafeV + rotateV;
+        v3 = driveV - strafeV - rotateV;
+        v4 = driveV + strafeV - rotateV;
+
+        leftFront.setPower(v1 * v1);
+        leftBack.setPower(v2 * v2);
+        rightFront.setPower(v3 * v3);
+        rightBack.setPower(v4 * v4);
+    }
+
+    public void runLift() {
+        if (this.gamepad1.left_trigger != 0) {
+            liftLeftV = this.gamepad1.left_trigger;
+            liftRightV = -this.gamepad1.left_trigger;
+
+            liftLeft.setPower(liftLeftV);
+            liftRight.setPower(liftRightV);
+        }
+        if (this.gamepad1.left_stick_button) {
+            liftLeft.setPower(0);
+            liftRight.setPower(0);
+        }
+    }
 
     @Override
     public void runOpMode() {
-        mRobot.init();
-        debugProgram();
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        //liftLeft = hardwareMap.get(DcMotor.class, "liftLeft");
+        //liftRight = hardwareMap.get(DcMotor.class, "liftRight");
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+        //liftLeft.setDirection(DcMotor.Direction.FORWARD);
+        //liftRight.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
         waitForStart();
         runtime.reset();
 
-        runProgramLoop();
-    }
-
-    public void setDirectionMotors() {
-        leftFront.setDirection(DcMotor.Direction.FORWARD);
-        leftBack.setDirection(DcMotor.Direction.FORWARD);
-        rightFront.setDirection(DcMotor.Direction.FORWARD);
-        rightBack.setDirection(DcMotor.Direction.FORWARD);
-
-        liftLeft.setDirection(DcMotor.Direction.FORWARD);
-        liftRight.setDirection(DcMotor.Direction.FORWARD);
-    }
-
-    public void debugProgram() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-    }
-
-    public void runProgramLoop() {
         while (opModeIsActive()) {
-            runMechanumAngleMethod();
-//            runMechanumVectorMethod();
+            runMechanumVectorMethod();
+//            runLift();
+
             telemetry.addData("Status", "Running");
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+            telemetry.addData("left front power", v1 * v1);
+            telemetry.addData("left back power", v2 * v2);
+            telemetry.addData("right front power", v3 * v3);
+            telemetry.addData("right back power", v4 * v4);
+            telemetry.addData("lift left power", liftLeftV);
+            telemetry.addData("lift right power", liftRightV);
+
             telemetry.update();
         }
-    }
-
-    public void setMotorPowers() {
-        leftFront.setPower(v1);
-        rightFront.setPower(v2);
-        leftBack.setPower(v3);
-        rightBack.setPower(v4);
-    }
-
-    public void runMechanumAngleMethod() {
-        // getting the distance from the origin of the joystick
-        double r = Math.hypot(this.gamepad1.left_stick_x, this.gamepad1.left_stick_y);
-        // angle of the joystick position from the origing
-        double robotAngle = Math.atan2(this.gamepad1.left_stick_y, this.gamepad1.left_stick_x) - Math.PI / 4;
-        //
-        double rightX = this.gamepad1.right_stick_x;
-        // scale the magnitude by the x and y values of the left angle stick
-        final double v1 = r * Math.cos(robotAngle) + rightX;
-        final double v2 = r * Math.sin(robotAngle) - rightX;
-        final double v3 = r * Math.sin(robotAngle) + rightX;
-        final double v4 = r * Math.cos(robotAngle) - rightX;
-
-        setMotorPowers();
-    }
-
-    public void runMechanumVectorMethod() {
-        double driveV, strafeV, rotateV;
-        driveV = -this.gamepad1.left_stick_x;
-        strafeV = this.gamepad1.left_stick_x;
-        rotateV = this.gamepad1.right_stick_x;
-
-        final double v1 = driveV + strafeV + rotateV;
-        final double v2 = driveV - strafeV + rotateV;
-        final double v3 = driveV - strafeV - rotateV;
-        final double v4 = driveV + strafeV - rotateV;
-
-        setMotorPowers();
     }
 }
