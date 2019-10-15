ada7a28c17add83d588120c568201826eb641b23
J <41495824+Jedite@users.noreply.github.com>
2019-10-14 21:55:02 -0700

    10/14

--- Finalized/DriverControl.java
+++ Finalized/DriverControl.java
@@ -3,9 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import java.lang.Math;
 
 @TeleOp
 //will display this opmode in the driver station
@@ -18,10 +16,38 @@ public class DriverControl extends LinearOpMode {
     private DcMotor rightFront;
     private DcMotor rightBack;
 
-    double v1, v2, v3, v4;
+    private DcMotor liftM;
 
-    //private DcMotor liftLeft;
-    //private DcMotor liftRight;
+    double v1, v2, v3, v4, liftV;
+
+    public void runMechanumVectorMethod() {
+        double driveV, strafeV, rotateV;
+
+        driveV = this.gamepad1.left_stick_y;
+        strafeV = this.gamepad1.left_stick_x;
+        rotateV = this.gamepad1.right_stick_x;
+
+        v1 = driveV + strafeV + rotateV;
+        v2 = driveV - strafeV + rotateV;
+        v3 = driveV - strafeV - rotateV;
+        v4 = driveV + strafeV - rotateV;
+
+        leftFront.setPower(v1);
+        leftBack.setPower(v2);
+        rightFront.setPower(v3);
+        rightBack.setPower(v4);
+    }
+
+    public void runLift() {
+        if (this.gamepad1.left_trigger != 0) {
+            liftV = this.gamepad1.left_trigger;
+
+            liftM.setPower(liftV);
+        }
+        if (this.gamepad1.left_stick_button) {
+            liftM.setPower(0);
+        }
+    }
 
     @Override
     public void runOpMode() {
@@ -36,7 +62,7 @@ public class DriverControl extends LinearOpMode {
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         leftBack.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
-        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
 
         //liftLeft.setDirection(DcMotor.Direction.FORWARD);
         //liftRight.setDirection(DcMotor.Direction.FORWARD);
@@ -49,6 +75,7 @@ public class DriverControl extends LinearOpMode {
 
         while (opModeIsActive()) {
             runMechanumVectorMethod();
+//            runLift();
 
             telemetry.addData("Status", "Running");
             telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -57,25 +84,9 @@ public class DriverControl extends LinearOpMode {
             telemetry.addData("left back power", v2);
             telemetry.addData("right front power", v3);
             telemetry.addData("right back power", v4);
+            telemetry.addData("lift power", liftV);
+
             telemetry.update();
         }
-    }
-    
-    public void runMechanumVectorMethod() {
-        double driveV, strafeV, rotateV;
-
-        driveV = -this.gamepad1.left_stick_y;
-        strafeV = -this.gamepad1.left_stick_x;
-        rotateV = this.gamepad1.right_stick_x;
-
-        v1 = driveV + strafeV + rotateV;
-        v2 = driveV - strafeV + rotateV;
-        v3 = driveV - strafeV - rotateV;
-        v4 = driveV + strafeV - rotateV;
-
-        leftFront.setPower(v1);
-        leftBack.setPower(v2);
-        rightFront.setPower(v3);
-        rightBack.setPower(v4);
     }
 }
